7a14e9428341a757068bd8bb28666588c5804229
GuhPlays <carvalhogustavo444@gmail.com>
2022-08-21 19:35:41 -0300

    Primeiro Committ

---
+++ ttss.js
@@ -0,0 +1,39 @@
+var ttss = [
+	{
+		"word":"brasileiro",
+		"clue":"Aquele que nasce no Brasil - masculino"
+	},
+	{
+		"word":"franceses",
+		"clue":"Um grupo de nascidos na França. "
+	},
+	{
+		"word":"euro-asiática",
+		"clue":"Aquela que é da Europa e da Ásia - feminino"
+	},
+	{
+		"word":"maranhense",
+		"clue":"Pessoa nascida no maranhão"
+	},
+	{
+		"word":"paraguaios",
+		"clue":"Produtos que vieram do paraguai"
+	},
+	{
+		"word":"japonês",
+		"clue":"Homem nascido no Japão"
+	},
+	{
+		"word":"paraense",
+		"clue":"Camarão proveniente do Pará"
+	},
+	{
+		"word":"britânicas",
+		"clue":"Conjunto de xícaras da Grã-Bretanha"
+	},
+	{
+		"word":"mato-grossense",
+		"clue":"Locativo (Estado de nascença) da Vanessa da Mata"
+	},
+	
+]
